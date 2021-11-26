7bd3b912461d407f48d87f7bc859a3c3a79b47d9
Glitch (serie-node) <none>
2021-11-26 03:49:28 +0000

    ✔️ episode 1 successfully completed

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("./controllers/authController.js")(app);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000");
+});
